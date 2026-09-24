98f642b749a057ab804f404f9f19b3140e63d2db
ElomMaio98 <estudos.elom@gmail.com>
2026-09-23 23:12:53 -0300

    finalização DML

--- transform/03_populate_core.sql
+++ transform/03_populate_core.sql
@@ -82,16 +82,40 @@ ON CONFLICT (name) DO NOTHING;
 INSERT INTO core.occurrence(id,document_number, occurred_at, address, latitude,longitude, 
 police_action,agent_presence,massacre,neighborhood_id,main_reason_id)
 SELECT
-    payload ->> 'id'
-    , payload ->> 'documentNumber'
-    , payload ->> 'date'
+    cast(payload ->> 'id' as UUID) as id
+    , cast(payload ->> 'documentNumber' as int) as documentNumber
+    , cast(payload ->>'date' as TIMESTAMPTZ) as date
     , payload ->> 'address'
-    , payload ->> 'latitude'
-    , payload ->> 'longitude'
-    , payload ->> 'policeAction'
-    , payload ->> 'agentPresence'
-    , payload ->> 'massacre'
-    , payload -> 'neighborhood' ->> 'id'
-    , payload -> 'mainReason' ->> 'id'
+    , cast(payload ->> 'latitude' as numeric) as latitude
+    , cast(payload ->> 'longitude' as numeric) as longitude
+    , cast(payload ->> 'policeAction' as boolean) as policeAction
+    , cast(payload ->> 'agentPresence'as boolean) as agentPresence
+    , cast(payload -> 'contextInfo' ->> 'massacre'as boolean) as massacre
+    , cast(payload -> 'neighborhood' ->> 'id'as UUID) as neighborhoodId
+    , cast(payload -> 'contextInfo' -> 'mainReason' ->> 'id'as UUID) as mainReasonId
 FROM public.raw_ocorrencias 
 ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO core.victim (id, occurrence_id, age, death_date, genre_id, age_group_id, situation_id, person_type_id)
+SELECT
+      cast(v ->> 'id' AS UUID)
+    , cast(v ->> 'occurrenceId' AS UUID)
+    , cast(v ->> 'age' AS INT)
+    , cast(v ->> 'deathDate' AS TIMESTAMPTZ)
+    , cast(v -> 'genre' ->> 'id' AS UUID)
+    , cast(v -> 'ageGroup' ->> 'id' AS UUID)
+    , s.id
+    , p.id
+FROM public.raw_ocorrencias o,
+     jsonb_array_elements(o.payload -> 'victims') AS v
+LEFT JOIN core.situation   s ON s.name = v ->> 'situation'
+LEFT JOIN core.person_type p ON p.name = v ->> 'personType'
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO core.occurrence_clipping (occurrence_id, clipping_id)
+SELECT DISTINCT
+      cast(o.payload ->> 'id' AS UUID)
+    , cast(c ->> 'id' AS UUID)
+FROM public.raw_ocorrencias o,
+     jsonb_array_elements(o.payload -> 'contextInfo' -> 'clippings') AS c
+ON CONFLICT (occurrence_id, clipping_id) DO NOTHING;
